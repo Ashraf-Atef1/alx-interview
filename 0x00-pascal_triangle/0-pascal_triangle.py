4f8ffb54cd135bb4a45eac29f67decbeb959cebd
AshrafAtef <alxashraf0@gmail.com>
2024-07-25 12:50:19 +0300

    pacal triangle task

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+0-pascal_triangle
+"""
+def pascal_triangle(n):
+    """
+    Returns a list of lists of integers representing the Pascal’s triangle of n
+    """
+    if n <= 0:
+        return []
+    triangle = []
+    for line in range(1, n + 1):
+        row = []
+        counter = 1
+        for i in range(1, line + 1):
+            row.append(counter)
+            counter = int(counter * (line - i) / i)
+        triangle.append(row)
+    return triangle
