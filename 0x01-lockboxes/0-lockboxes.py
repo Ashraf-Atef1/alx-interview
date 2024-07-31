a582ade93a4585fcb4a8b1b17c856d50d5143c8a
AshrafAtef <alxashraf0@gmail.com>
2024-07-31 05:06:50 +0300

    upload open box task

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''This module contains a function that checks if all the boxes can be opened'''
+
+def open_box(boxes, number, numbers_list):
+    '''This function opens the boxes and checks if all the boxes can be opened'''
+    numbers_list.add(number)
+    for current_number in boxes[number]:
+        if current_number not in numbers_list:
+            open_box(boxes, current_number, numbers_list)
+
+def canUnlockAll(boxes):
+    '''This function checks if all the boxes can be opened'''
+    numbers_list = set()
+    open_box(boxes, 0, numbers_list)
+    if len(numbers_list) == len(boxes):
+        return True
