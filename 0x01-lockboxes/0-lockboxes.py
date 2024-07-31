3cd9854b5ea6b9e0c88e9ec4161e400cf55c9ae5
AshrafAtef <alxashraf0@gmail.com>
2024-07-31 05:28:16 +0300

    upload open box task

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -9,14 +9,16 @@ def open_box(boxes, number, numbers_list):
     if number < 0 or number >= len(boxes):
         return
     numbers_list.add(number)
-    for current_number in boxes[number]:
-        if isinstance(current_number, int) and current_number not in numbers_list:
-            open_box(boxes, current_number, numbers_list)
+    for c_number in boxes[number]:
+        if isinstance(c_number, int) and c_number not in numbers_list:
+            open_box(boxes, c_number, numbers_list)
 
 
 def canUnlockAll(boxes):
     '''This function checks if all the boxes can be opened'''
     numbers_list = set()
+    if len(boxes) == 0:
+        return False
     open_box(boxes, 0, numbers_list)
     if len(numbers_list) == len(boxes):
         return True
